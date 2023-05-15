767267ef3515dd83e750c30c691110667808ddf1
Cherifi Imane <50464518+CherifiImene@users.noreply.github.com>
2023-05-15 21:48:39 +0200

    Implemented the preprocessing function

--- data/pipeline.py
+++ data/pipeline.py
@@ -1,22 +1,33 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, count
+from pyspark.sql.functions import(
+    col, 
+    Row,
+    count, 
+    countDistinct
+)
+from pyspark.sql.types import(
+     StructType,
+     StructField,
+     StringType,
+     ArrayType,
+     IntegerType
+     )
 
+import os
 
-
-spark = SparkSession.builder\
+class DataPipeline:
+    spark = SparkSession.builder\
         .master("local")\
         .appName("Dressipi-SBRS")\
         .config('spark.ui.port', '4050')\
         .getOrCreate()
-
-
-class DataPipeline:
+        
     def __init__(self,path_to_data) -> None:
 
         self.path_to_data = path_to_data
         pass
     
-    
+    #------- ETL -------#
     def extract(self,table="train_sessions.csv"):
         table_path = os.path.join(self.path_to_data,table)
         data = spark.read.format("csv")\
@@ -24,6 +35,16 @@ class DataPipeline:
                     .load(table_path)
         return data
     
+    def create_rdd(self,data):
+        schema = StructType([
+                  StructField('item_id', StringType(), True),#column_name,data_type,nullable,metadata
+                  StructField('features', ArrayType(IntegerType()), False),
+                  ])
+        #empty_rdd = spark.sparkContext.emptyRDD()
+
+        rdd = spark.createDataFrame(data,schema)
+        return rdd
+
     def transform(self):
         pass
     
@@ -36,22 +57,56 @@ class DataPipeline:
                     .load(clean_data_path)
         return clean_data
     
-    def preprocess_dataset(self):
+    def save(self,data,dest_folder,dest_file):
+        dest = os.path.join(self.path_to_data,
+                            dest_folder)
+        data.write.option("header",True)\
+            .csv(dest)
+        # rename saved file
+        spark_out = list(filter(
+            lambda filename: filename.startswith('part-'),
+            os.listdir(dest)))[0]
+        os.rename(dest+'/'+spark_out,dest+'/'+dest_file)
+
+    #---------------------------#
+    
+    def preprocess_dataset(self,clean_data=False):
 
         # 1. clean dataset 
         # (remove duplicates from features)
-        self.clean_dataset()
+        if clean_data:
+          self.clean_dataset()
         
-        # 2. Load clean data
-        clean_data = self.load(
-            clean_data_path="clean_item_features.csv"
-            )
+        # 2. Load candidate items 
+        # transform them to (item_id, features_vector)
+        candidates = self.extract(table="candidate_items.csv").collect()
+        clean_features = self.load(table="clean_item_features/features.csv")
+
+        preprocessed_candidates = []
+
+        for candidate in candidates:
+          item_features = clean_features.select(
+              ["item_id","feature_category_id"]
+              ).where(f"item_id == {candidate.item_id}")\
+              .collect()
+          num_features = [0]*len(item_features)
+          for feature in item_features:
+            num_features[feature.feature_category_id-1] += 1
+          
+          preprocessed_candidates.append([candidate.item_id,
+                                          num_features])
+        preprocessed_df = self.create_rdd(preprocessed_candidates)
+
+        # 5. save new data
+        self.save(data=preprocessed_df,
+                  dest_folder="prep_candidates",
+                  dest_file="candidates")
         
         # 3. transform item to vector of features
 
         # 4. remove duplicate vectors
 
-        # 5. save new data
+       
 
         pass
     
@@ -60,12 +115,20 @@ class DataPipeline:
         data = self.extract(table="item_features.csv")
         
         # transform data
-        data = data.dropDuplicates(["feature_category_id"])
-        print(f'Distinct count of categories & items :\
-                {data.feature_category_id.count()},\
-                {data.item_id.count()}')
+        data = data.orderBy(col("item_id"))\
+                   .dropDuplicates(["item_id","feature_category_id"])
+        
+        #data.select(count(data.feature_category_id)).show()
+        #data.select(count(data.item_id)).show()
+        
         
         # save cleaned features
         dest = os.path.join(self.path_to_data,
-                            "clean_item_features.csv")
-        data.write.csv(dest)
+                            "clean_item_features")
+        data.write.option("header",True)\
+            .csv(dest)
+        # rename saved file
+        spark_out = list(filter(
+            lambda filename: filename.startswith('part-'),
+            os.listdir(dest)))[0]
+        os.rename(dest+'/'+spark_out,dest+'/'+'features.csv')
