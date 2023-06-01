bb98bc770db3149a8b781fdfb70665008cc72e94
Cherifi Imane <50464518+CherifiImene@users.noreply.github.com>
2023-06-01 20:19:57 +0200

    Added clustering algorithms

---
+++ utils/models.py
@@ -0,0 +1,20 @@
+
+
+
+class LOF:
+    def __init__(self) -> None:
+        pass
+
+
+
+class K_MEANS:
+    def __init__(self) -> None:
+        pass
+
+
+
+class K_MEDOID:
+    
+    def __init__(self) -> None:
+        pass
+
