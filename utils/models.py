ee901ab16c0e35823c09095b30e250e067e15d66
Cherifi Imane <50464518+CherifiImene@users.noreply.github.com>
2023-06-07 23:59:34 +0200

    Update models.py

--- utils/models.py
+++ utils/models.py
@@ -1,31 +1,35 @@
 from math import inf
 import random as rd
 import numpy as np
-from metrics.hamming_distance import HammingDistance
 
 class K_MODES:
     
     def __init__(self,k=2) -> None:
-        # verify k>=2
-        self.k = k
+      # verify k>=2
+      self.k = k
     
 
     def fit(self,X):
       
       # Choose the latest(0) and oldest items(-1)
       # as starting modes
-      modes = [X[0],X[-1]]
+      # if the number of elements in a cluster is 1
+      # verify wether the cluster can be concatenated with another cluster
+      modes = np.array([X[0,:],X[-1,:]])
 
       if self.k >2:
-        modes.extend(rd.choices(X,k=self.k-2))
+        modes = np.append(modes,
+                        [rd.choices(X,k=self.k-2)],
+                        axis=0)
       
-      ishifted = True
+      item_shifted = True
       c_objects = None
       while item_shifted:
         c_objects, item_shifted = self._assign_objects(X,modes,
                                                        c_objects)
         modes = self._update_modes(c_objects)
-        X = list(c_objects.values())
+        print(f"Type(modes): {type(modes)}")
+      return c_objects, modes
 
     def _assign_objects(self,X,modes,
                         c_objects=None):
@@ -39,7 +43,7 @@ class K_MODES:
         clusters_members = c_objects.items()
         for cluster, items in clusters_members:
 
-          for item in items:
+          for index,item in enumerate(items):
             distances = [HammingDistance\
                       .evaluate(item,
                                 modes[c]) for c in clusters]
@@ -47,14 +51,20 @@ class K_MODES:
             
             if not condition:
               condition = n_cluster != cluster
+              print(f"Condition state changed to : {condition}")
             
-            if condition:
+            elif (n_cluster != cluster):
+              print(f"Item : {index}, Clusters are different: old : {cluster}, new: {n_cluster}")
               # remove from old cluster
-              c_objects[cluster].remove(item)
+
+              '''Issue while using del to delete the item, they are not being deleted '''
               
+              c_objects[cluster][index] = c_objects[cluster][-1]
+              c_objects[cluster][-1] = -1
+              del c_objects[cluster][-1]
+             
               # assign to new cluster
               c_objects[n_cluster].append(item)
-
       return c_objects, condition
     
     def _first_allocation(self,X,modes):
@@ -76,36 +86,35 @@ class K_MODES:
       return c_objects, condition
 
     def _update_modes(self,c_objects):
+      # Make sure that the modes are numpy array
       new_modes = []
       for cluster in c_objects.keys():
         items = np.array(c_objects[cluster])
+        #print(f"items.shape={items.shape}")
         nb_items,nb_features = items.shape
 
         # compute the new mode 
         n_mode = []
         for feature in range(nb_features):
+          # validate that the number of categories is at least 2
           categories = np.unique(items[:,feature])
-          frequencies = [np.count(items == category)/nb_items\
+          #print(f"categories: {categories}")
+          if len(categories <2):
+            categories = range(2)
+          # add epsilon to avoid division by zero
+          frequencies = [np.count_nonzero(items[:,feature] == category,axis=0)/nb_items\
                          for category in categories]
           
+          #print(f"Frequencies: {frequencies}")
           n_mode.append(np.argmax(frequencies))
-        new_modes.append(n_mode)
+        assert len(n_mode) == 73, f"Expected mode to be list of features : {len(n_mode)}"
+        new_modes.append(np.array(n_mode))
       
-      return new_modes
+      return np.array(new_modes)
           
       
         
 
 
       
-
-class LOF:
-    def __init__(self) -> None:
-        pass
-
-
-class K_MEDOID:
-    
-    def __init__(self) -> None:
-        pass
 
